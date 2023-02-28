2b4547aa4cf375b81a3f9a332fea16c94497af80
“ITStanley” <isharastanley2000@gmail.com>
2023-02-28 22:14:01 +0530

    I have added the home page

---
+++ js/mscript.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	$(window).scroll(function(){
+		
+    if(this.scrollY > 20){
+			$('.navbar').addClass("sticky");
+		}else{
+            $('.navbar').removeClass("sticky");
+		}
+
+    if(this.scrollY > 200){
+      $('.scroll-up-btn').addClass("show");
+    }else{
+      $('.scroll-up-btn').removeClass("show");
+    }
+	});
+
+  // slide-up script
+  $('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+  });
+
+  // var typed = new Typed(".typing", {
+  //    strings: ["Youtuber", "Devalopper", "Blogger", "Dedingner"],
+  //    typeSpeed: 100,
+  //    backSpeed: 60,
+  //    loop:true
+  // });
+
+	// togle menu/navbar script
+	$('.menu-btn').click(function(){
+		$('.navbar .menu').toggleClass("active");
+		$('.menu-btn i').toggleClass("active");
+	});
+});
+
+
+
+
