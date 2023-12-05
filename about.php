7de31e8b1c55ca4729db31864d1b5fe13224fae1
Tharindu <t.n.hettige@gmail.com>
2023-12-05 15:57:00 +0530

    minor code changes

--- about.php
+++ about.php
@@ -48,6 +48,7 @@ if ($conn->connect_error) {
                 <li><a href="index.php#explore">Explore</a></li>
                 <li><a href="about.php">About</a></li>
                 <li><a href="contact.php">Contact</a></li>
+                <li><a href="contact.php">Admin Panel</a></li>
             </ul>
         </div>
     </nav>
@@ -119,6 +120,7 @@ if ($conn->connect_error) {
                 <a href="index.php#explore">Explore</a>
                 <a href="about.php">About</a>
                 <a href="contact.php">Contact</a>
+                <a href="contact.php">Admin Panel</a>
 
             </p>
             <p class="footer-company-name">TravelBug © 2022</p>
--- hik_cp_details.php
+++ hik_cp_details.php
@@ -49,6 +49,7 @@ $result = $conn->query($sql);
                 <li><a href="index.php#explore">Explore</a></li>
                 <li><a href="about.php">About</a></li>
                 <li><a href="contact.php">Contact</a></li>
+                <li><a href="contact.php">Admin Panel</a></li>
             </ul>
         </div>
     </nav>
@@ -2611,6 +2612,7 @@ if ($result->num_rows > 0) {
                 <a href="index.php#explore">Explore</a>
                 <a href="about.php">About</a>
                 <a href="contact.php">Contact</a>
+                <a href="contact.php">Admin Panel</a>
 
             </p>
             <p class="footer-company-name">TravelBug © 2022</p>
